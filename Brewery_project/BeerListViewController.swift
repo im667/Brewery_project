a3485e465628e2214bb3624d3fff6e70d554669d
im667 <koh9037@gmail.com>
2022-05-05 03:25:28 +0900

    ADD: prefetching Data

--- Brewery_project/BeerListViewController.swift
+++ Brewery_project/BeerListViewController.swift
@@ -11,6 +11,7 @@ import Kingfisher
 class BeerListViewController: UITableViewController {
     
     var beerList = [Beer]()
+    var dataTasks = [URLSessionTask]()
     var currentPage = 1
     
     override func viewDidLoad() {
@@ -24,6 +25,7 @@ class BeerListViewController: UITableViewController {
         
         tableView.register(BeerListCell.self, forCellReuseIdentifier: BeerListCell.identifier)
         tableView.rowHeight  = 150
+        tableView.prefetchDataSource = self
         
         fetchBeer(of: currentPage)
     }
@@ -32,7 +34,7 @@ class BeerListViewController: UITableViewController {
 }
 
 
-extension BeerListViewController {
+extension BeerListViewController:UITableViewDataSourcePrefetching {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return beerList.count
@@ -56,13 +58,27 @@ extension BeerListViewController {
         detailViewController.beer = selectedBeer
         self.show(detailViewController, sender: nil)
     }
+    
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        
+            guard currentPage != 1 else { return }
+        indexPaths.forEach {
+            if ($0.row + 1 ) / 25 + 1 == currentPage {
+                self.fetchBeer(of: currentPage)
+            }
+        }
+    }
+    
 }
 
 //data Fetching
 private extension BeerListViewController {
     
     func fetchBeer(of page:Int){
-        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)") else { return }
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)"),
+              dataTasks.firstIndex(where: {
+                  $0.originalRequest?.url == url }) == nil else { return }
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -104,7 +120,7 @@ private extension BeerListViewController {
         
         }
         dataTask.resume()
-        
+        dataTasks.append(dataTask)
                 
     }
     
